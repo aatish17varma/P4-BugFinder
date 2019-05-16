74fdc8c9ee06e1c9821dccd8ed44ddc7ddcbd643
Peixuan Gao <peixuan670@outlook.com>
2019-05-16 18:22:17 -0400

    new scapy script fixed bug (port number)

--- scapy/demo9_bmv2_simple_switch_scapy_test.py
+++ scapy/demo9_bmv2_simple_switch_scapy_test.py
@@ -13,22 +13,22 @@ data="this is a test"
 p1 = Ether()/IP(src="128.0.0.0",dst="10.0.0.1")/TCP(sport=1234,dport=1)/data
 
 # Sending the constructed packet into the interface veths
-sendp(p1,iface="veth0")
+sendp(p1,iface="veth2")
 
 
 p2 = Ether()/IP(src="128.0.0.1",dst="10.0.0.2")/TCP(sport=1234,dport=2)/data
-sendp(p2,iface="veth0")
+sendp(p2,iface="veth2")
 
 
 p3 = Ether()/IP(src="10.0.0.3",dst="128.0.0.1")/TCP(sport=1234,dport=3)/data
-sendp(p3,iface="veth0")
+sendp(p3,iface="veth2")
 
 
 p4 = Ether()/IP(src="10.0.0.0",dst="128.0.0.1")/TCP(sport=1234,dport=10)/data
-sendp(p4,iface="veth0")
+sendp(p4,iface="veth2")
 
 p4 = Ether()/IP(src="10.0.0.0",dst="255.255.255.255")/TCP(sport=1234,dport=10)/data
-sendp(p4,iface="veth0")
+sendp(p4,iface="veth2")
 
 # Use sniff() to capture packets
 #sniff(filter="",iface="any", prn=function, count=N)
